13898675de37f626f8ce6dd1b64c62b4871f2b5f
bagasadit123 <bagasadit430@gmail.com>
2026-03-02 20:41:10 +0700

    prosedur input data

--- KuisChallenge.cpp
+++ KuisChallenge.cpp
@@ -2,3 +2,12 @@
 using namespace std;
 
 float Luas, D1, D2;
+
+void ProsedurInputData(){
+    cout << "Masukkan diagonal 1 : ";
+    cin >> D1;
+    cout << "Masukkan diagonal 2 : ";
+    cin >> D2;
+
+}
+

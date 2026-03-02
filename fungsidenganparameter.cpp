e49b365a3133d7fdf483bd624f6a69d2c71f28ca
bagasadit123 <bagasadit430@gmail.com>
2026-03-02 21:21:53 +0700

    luas persegi, jumlah, output

--- fungsidenganparameter.cpp
+++ fungsidenganparameter.cpp
@@ -10,3 +10,12 @@ void input(){
     cout << "Masukkan Lebar : ";
     cin >> lebar;
 }
+int luasPersegi(int a,int b){
+    return a * b;
+}
+int jumlah(int x, int y, int z){
+    return x+y+z
+}
+void output(){
+    cout << "Hasilnya : " << luasPersegi(panjang,lebar) << endl;
+}
